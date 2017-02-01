68ae8430f201d2bc3b2dcf2126525fe4e9891f58
zstao <zstao1111@foxmail.com>
2017-02-01 21:34:50 +0800

    图片上传接口

--- src/main/java/com/kabuda/controller/FileController.java
+++ src/main/java/com/kabuda/controller/FileController.java
@@ -2,19 +2,22 @@ package com.kabuda.controller;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.kabuda.entity.User;
 import com.kabuda.entity.domain.Response;
+import com.kabuda.service.UserService;
 import com.kabuda.util.ResponseCode;
-import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.context.ContextLoader;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
+import java.io.IOException;
 import java.util.*;
 
 
@@ -22,67 +25,52 @@ import java.util.*;
 @RequestMapping("/file")
 public class FileController {
 
+    private final UserService userService;
+
+    @Autowired
+    public FileController(UserService userService) {
+        this.userService = userService;
+    }
+
 
     @ResponseBody
     @RequestMapping("/upload")
-    public String upload(HttpServletRequest request,String id) {
+    public String upload(Integer type, Integer id, HttpServletRequest request) {
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        String realPath = request.getSession().getServletContext().getRealPath("/")+"/" + "file";
-        File filePath = new File(realPath);
-        if(!filePath.exists() && !filePath.isDirectory()){
-            filePath.mkdir();
-        }
         try {
+            if (StringUtils.isEmpty(type) || type < 0 || type > 1 ||
+                    (type == 0 && StringUtils.isEmpty(id))) {
+                return gson.toJson(new Response(ResponseCode.R_1001));
+            }
+
+            User user = (User) request.getSession().getAttribute("user");
+            if (user == null) {
+                return gson.toJson(new Response(ResponseCode.R_1010));
+            }
+
+            String realPath = request.getSession().getServletContext().getRealPath("/");
+
+            initFile(realPath);
+
             CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver(request.getSession().getServletContext());
             //判断 request 是否有文件上传,即多部分请求
-            if(multipartResolver.isMultipart(request)){
+            if (multipartResolver.isMultipart(request)) {
                 MultipartHttpServletRequest multiRequest = (MultipartHttpServletRequest) request;
                 Iterator<String> iterator = multiRequest.getFileNames();
-                while(iterator.hasNext()){
-                    //记录上传过程起始时的时间，用来计算上传时间
-                    //int pre = (int) System.currentTimeMillis();
+                while (iterator.hasNext()) {
                     //取得上传文件
                     MultipartFile file = multiRequest.getFile(iterator.next());
-                    if(file != null){
+                    if (file != null) {
                         //取得当前上传文件的文件名称
                         String myFileName = file.getOriginalFilename();
-                        String timeStamp = String.valueOf(System.currentTimeMillis());
-                        String prefix=myFileName.substring(myFileName.lastIndexOf(".")+1);
-                        myFileName = timeStamp +"." +prefix;
+
                         //如果名称不为“”,说明该文件存在，否则说明该文件不存在
-                        if(!StringUtils.isEmpty(myFileName.trim())){
-//                            System.out.println(myFileName);
-//                            //重命名上传后的文件名
-//                            String fileName = file.getOriginalFilename();
-//                            //定义上传路径
-                            String path = realPath + "\\" + myFileName;
-                            File localFile = new File(path);
-                            file.transferTo(localFile);
-                            Map result =  new HashMap();
-                            List<String> strings = new ArrayList<String>();
-                            strings.add("file/"+myFileName);
-                            result.put("initialPreview",strings);
-
-
-
-                            List arrayList = new ArrayList();
-
-                            Map initialPreviewConfig = new HashMap();
-                            initialPreviewConfig.put("url","delete-url");
-                            initialPreviewConfig.put("key","1,0");
-                            initialPreviewConfig.put("size",file.getSize());
-                            initialPreviewConfig.put("caption",myFileName);
-                            arrayList.add(initialPreviewConfig);
-
-                            result.put("initialPreviewConfig",arrayList);
-                            return gson.toJson(result);
+                        if (!StringUtils.isEmpty(myFileName.trim())) {
+                            return saveFile(file, realPath, type, request);
                         }
-                    }else{
+                    } else {
                         return gson.toJson(new Response(ResponseCode.R_1100));
                     }
-                    //记录上传该文件后的时间
-                    //int finalTime = (int) System.currentTimeMillis();
-                    //System.out.println(finalTime - pre);
                 }
                 return gson.toJson(new Response(ResponseCode.R_1000));
             }
@@ -91,5 +79,67 @@ public class FileController {
             e.printStackTrace();
             return gson.toJson(new Response(ResponseCode.R_1100));
         }
+    }
+
+
+    /**
+     * @param type 上传类型(车辆:0, 头像:1)
+     */
+    private String saveFile(MultipartFile file, String realPath, int type, HttpServletRequest request) throws IOException {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+        String timeStamp = String.valueOf(System.currentTimeMillis());
+        String myFileName = timeStamp + "_" + file.getOriginalFilename();
+
+        // 车辆: 0, 头像: 1
+        if (type == 1) {
+            // 保存头像
+            String relativePath = "/file/avatar/" + myFileName;
+            String path = realPath + relativePath;
+            File localFile = new File(path);
+            file.transferTo(localFile);
+            // 更新数据库
+            User user = (User) request.getSession().getAttribute("user");
+            user.setHeadPotrait(relativePath);
+            userService.update(user);
+            request.getSession().removeAttribute("user");
+            request.getSession().setAttribute("user", user);
+            return gson.toJson(new Response(ResponseCode.R_1000));
+        } else {
+            String relativePath = "/file/vehicle/" + myFileName;
+            String path = realPath + relativePath;
+            File localFile = new File(path);
+            file.transferTo(localFile);
+
+
+            Map result = new HashMap();
+            List<String> strings = new ArrayList<String>();
+            strings.add("file/" + myFileName);
+            result.put("initialPreview", strings);
+
+            List arrayList = new ArrayList();
+
+            Map initialPreviewConfig = new HashMap();
+            initialPreviewConfig.put("caption", myFileName);
+            initialPreviewConfig.put("url", "delete-url");
+            initialPreviewConfig.put("key", "1,0");
+            initialPreviewConfig.put("size", file.getSize());
+            arrayList.add(initialPreviewConfig);
+
+            result.put("initialPreviewConfig", arrayList);
+            return gson.toJson(new Response(ResponseCode.R_1000));
+        }
+    }
+
+    private void initFile(String realPath) {
+
+        File filePath = new File(realPath + "file");
+        if (!filePath.exists() && !filePath.isDirectory()) filePath.mkdir();
+
+        File avatarPath = new File(realPath + "file/avatar");
+        if (!avatarPath.exists() && !avatarPath.isDirectory()) avatarPath.mkdir();
+
+        File vehiclePath = new File(realPath + "file/vehicle");
+        if (!vehiclePath.exists() && !vehiclePath.isDirectory()) vehiclePath.mkdir();
     }
 }
